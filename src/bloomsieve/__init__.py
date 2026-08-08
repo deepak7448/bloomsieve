2a98ebb94b889181efef10718b0857e2844861f3
deepak prajapati <deepak.prajapati@voxturrlabs.com>
2026-08-08 15:10:23 +0530

    chore: simplify README, update metadata, and add build script for packaging

--- src/bloomsieve/__init__.py
+++ src/bloomsieve/__init__.py
@@ -2,6 +2,6 @@ from .core import BloomFilter
 from .redis_service import BloomFilterService
 from .utils import get_hash_indices, get_optimal_m_k
 
-__version__ = "0.1.2"
+__version__ = "0.1.4"
 
 __all__ = ["BloomFilter", "BloomFilterService", "get_hash_indices", "get_optimal_m_k", "__version__"]
